f67fdcfaedae73e2e291b3d33579501aff8a085d
wonhyuk yang <wonhyuk@postech.ac.kr>
2024-09-05 15:17:27 +0000

    [MLIR] fix vpop encoding

--- mlir/test/lib/Conversion/PyTorchSimToVCIX/TestPyTorchSimToVCIXConversion.cpp
+++ mlir/test/lib/Conversion/PyTorchSimToVCIX/TestPyTorchSimToVCIXConversion.cpp
@@ -135,7 +135,7 @@ struct MatmulOpLowering : public OpRewritePattern<linalg::MatmulOp> {
     Attribute vipush_opcode =  rewriter.getI64IntegerAttr(0b000000);
     Attribute vwpush_opcode =  rewriter.getI64IntegerAttr(0b000001);
     Attribute compute_opcode = rewriter.getI64IntegerAttr(0b000001);
-    Attribute vpop_opcode = rewriter.getI64IntegerAttr(0b000001);
+    Attribute vpop_opcode = rewriter.getI64IntegerAttr(0b000010);
     Value rvl = nullptr;
 
     // For loop part
