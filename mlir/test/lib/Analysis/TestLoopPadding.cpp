3e46d2b1b99e19232f74724102a9ad87414d8828
wonhyuk yang <wonhyuk@postech.ac.kr>
2024-10-15 06:45:52 +0000

    [PyTorchSim] Fix padding and support 3D

--- mlir/test/lib/Analysis/TestLoopPadding.cpp
+++ mlir/test/lib/Analysis/TestLoopPadding.cpp
@@ -41,17 +41,29 @@ struct TestLoopPadding
   void setLoopUpperBound(affine::AffineForOp forOp, int64_t newUpperBound);
   void modifyMemrefWithPadding(Value memref, int64_t stepSize, int64_t paddedUpperBound);
   int64_t findSecondSmallestCoefficient(AffineExpr expr);
-  int findLoopVariableIndexInApplyOp(affine::AffineApplyOp applyOp, mlir::Value loopVar);
-  mlir::AffineExpr updateAffineExprWithBounds(mlir::AffineExpr expr, 
-                                            int updated_position_index, 
-                                            int64_t upperBound, 
-                                            int64_t paddedUpperBound, 
+  int findLoopVariableIndex(mlir::Value loopVar);
+  mlir::AffineExpr updateAffineExprWithBounds(mlir::AffineExpr expr,
+                                            int updated_position_index,
+                                            int64_t upperBound,
+                                            int64_t paddedUpperBound,
                                             mlir::MLIRContext *context);
-  AffineMap updateAffineMapWithNewExpr(AffineMap oldMap, 
-                                       mlir::AffineExpr updatedExpr, 
-                                       unsigned exprIndex, 
-                                       mlir::MLIRContext *context);
-  std::map<const void*, std::tuple<int64_t, int64_t>> loopInfoMap;
+  AffineMap updateAffineMapWithNewExpr(AffineMap& oldMap,
+                                       mlir::AffineExpr updatedExpr,
+                                       unsigned exprIndex,
+                                       mlir::MLIRContext *context,
+                                       affine::AffineForOp& forOp);
+                                       // Function to check if a loop is already registered
+  bool isLoopRegistered(AffineMap& map, affine::AffineForOp& forOp) {
+    // Check if the map exists in the loopMap
+    auto it = paddedMapInfo.find(map.getAsOpaquePointer());
+    if (it != paddedMapInfo.end()) {
+      // Check if the forOp is already in the set of loops for this map
+      return it->second.find(forOp.getAsOpaquePointer()) != it->second.end();
+    }
+    return false;  // Map not found, so forOp can't be registered
+  }
+  std::vector<std::tuple<int64_t, int64_t, Value>> loopInfoMap;
+  std::map<const void*, std::set<const void*>> paddedMapInfo;
 };
 
 } // namespace
@@ -67,7 +79,7 @@ void TestLoopPadding::runOnOperation() {
     int64_t upperBound = getLoopUpperBound(forOp);
     if (upperBound == -1)
         forOp.emitError() << "Unexpected loop upper size\n";
-    loopInfoMap[forOp.getAsOpaquePointer()] = std::make_tuple(upperBound, stepSize);
+    loopInfoMap.push_back(std::make_tuple(upperBound, stepSize, forOp.getInductionVar()));
 
     // Step 2: Modify the loop bounds to make upperBound a multiple of stepSize
     int64_t paddedUpperBound = roundUpToMultiple(upperBound, stepSize);
@@ -108,20 +120,28 @@ void TestLoopPadding::runOnOperation() {
         // Create a new function type with updated argument types
         auto newFuncType = FunctionType::get(function.getContext(), newArgTypes, funcType.getResults());
         function.setType(newFuncType);  // Set the new function type
- 
+
         int64_t mm_stride = -1;
+        int64_t new_mm_stride;
         for (auto operand : dmaOp.getOperands()) {
           if (auto applyOp = operand.getDefiningOp<mlir::affine::AffineApplyOp>()) {
-            auto index_pos = findLoopVariableIndexInApplyOp(applyOp, forOp.getInductionVar());
+            auto index_pos = findLoopVariableIndex(forOp.getInductionVar());
             AffineMap map = applyOp.getAffineMap();
             for (unsigned i = 0; i < map.getNumResults(); ++i) {
               AffineExpr expr = map.getResult(i);
 
               // Find old mm_stride
-              AffineExpr new_expr = updateAffineExprWithBounds(expr, index_pos, upperBound, paddedUpperBound, context);
-              map = updateAffineMapWithNewExpr(map, new_expr, i, context);
-              applyOp.setMap(map);
-              mm_stride = findSecondSmallestCoefficient(new_expr);
+              if (!isLoopRegistered(map, forOp)) {
+                expr = updateAffineExprWithBounds(expr, index_pos, upperBound, paddedUpperBound, context);
+                map = updateAffineMapWithNewExpr(map, expr, i, context, forOp);
+                applyOp.setMap(map);
+              }
+
+              auto strideVal = dmaOp.getStride().getDefiningOp<arith::ConstantIndexOp>();
+              if (strideVal && strideVal.value() != 1) {
+                new_mm_stride = findSecondSmallestCoefficient(expr);
+                mm_stride = mm_stride !=1 ? new_mm_stride : stepSize;
+              }
               //llvm::errs() << "old_expr: " << expr << " new_expr: " << new_expr << "\n";
               //llvm::errs() << "index_pos: " << index_pos << " upper: " << upperBound << " paddUpper: " << paddedUpperBound << "\n";
             }
@@ -172,7 +192,7 @@ void TestLoopPadding::modifyMemrefWithPadding(Value memref, int64_t upperBound, 
 
   // Check if the memref is 1D (one-dimensional)
   if (memrefType.getRank() != 1) {
-    memref.getDefiningOp()->emitError() << "Expected 1D memref, but got a memref of rank " 
+    memref.getDefiningOp()->emitError() << "Expected 1D memref, but got a memref of rank "
                                         << memrefType.getRank();
     return;
   }
@@ -199,11 +219,12 @@ int64_t TestLoopPadding::findSecondSmallestCoefficient(AffineExpr expr) {
       if (binExpr.getKind() == AffineExprKind::Mul) {
         if (auto constExpr = llvm::dyn_cast<AffineConstantExpr>(binExpr.getRHS())) {
           coefficients.push_back(constExpr.getValue());
-        } else {
-          coefficients.push_back(1);  // No explicit constant means coefficient is 1
+          return;
+        } else if (auto constExpr = llvm::dyn_cast<AffineConstantExpr>(binExpr.getLHS())) {
+          coefficients.push_back(constExpr.getValue());
+          return;
         }
       }
-      // Recursively handle both sides of the binary expression
       extractCoefficients(binExpr.getLHS());
       extractCoefficients(binExpr.getRHS());
     } else if (auto constExpr = llvm::dyn_cast<AffineConstantExpr>(expr)) {
@@ -221,32 +242,31 @@ int64_t TestLoopPadding::findSecondSmallestCoefficient(AffineExpr expr) {
     return -1;
   }
 
-  std::sort(coefficients.begin(), coefficients.end(), std::greater<int64_t>());
+  std::sort(coefficients.begin(), coefficients.end(), std::less<int64_t>());
+
+  //for (auto coe:coefficients)
+  //  llvm::errs() << coe << " ";
 
   return coefficients[1];
 }
 
-mlir::AffineExpr TestLoopPadding::updateAffineExprWithBounds(mlir::AffineExpr expr, 
-                                            int updated_position_index, 
-                                            int64_t upperBound, 
-                                            int64_t paddedUpperBound, 
+mlir::AffineExpr TestLoopPadding::updateAffineExprWithBounds(mlir::AffineExpr expr,
+                                            int updated_position_index,
+                                            int64_t upperBound,
+                                            int64_t paddedUpperBound,
                                             mlir::MLIRContext *context) {
   // Step 1: Traverse the AffineExpr to collect coefficients for each index
   SmallVector<std::tuple<int64_t, unsigned>, 4> coefficients;
-
   std::function<void(mlir::AffineExpr)> collectCoefficients = [&](mlir::AffineExpr expr) {
     if (auto binExpr = llvm::dyn_cast<mlir::AffineBinaryOpExpr>(expr)) {
-      // For binary expressions, handle mul and add differently
       if (binExpr.getKind() == mlir::AffineExprKind::Mul) {
         // Mul: Assume binExpr is in the form of constant * dimension (e.g., 47 * index0)
         if (auto lhs = llvm::dyn_cast<mlir::AffineConstantExpr>(binExpr.getLHS())) {
           if (auto rhs = llvm::dyn_cast<mlir::AffineDimExpr>(binExpr.getRHS())) {
-            // Extract coefficient and update the corresponding dimension
             coefficients.push_back(std::make_tuple(lhs.getValue(), rhs.getPosition()));
           }
         } else if (auto rhs = llvm::dyn_cast<mlir::AffineConstantExpr>(binExpr.getRHS())) {
           if (auto lhs = llvm::dyn_cast<mlir::AffineDimExpr>(binExpr.getLHS())) {
-            // Extract coefficient and update the corresponding dimension
             coefficients.push_back(std::make_tuple(rhs.getValue(), lhs.getPosition()));
           }
         }
@@ -256,7 +276,6 @@ mlir::AffineExpr TestLoopPadding::updateAffineExprWithBounds(mlir::AffineExpr ex
         collectCoefficients(binExpr.getRHS());
       }
     } else if (auto dimExpr = llvm::dyn_cast<mlir::AffineDimExpr>(expr)) {
-      // If it's a dimension expression (index variable), assign coefficient 1
       coefficients.push_back(std::make_tuple(1, dimExpr.getPosition()));
     }
   };
@@ -264,76 +283,96 @@ mlir::AffineExpr TestLoopPadding::updateAffineExprWithBounds(mlir::AffineExpr ex
   // Collect coefficients from the AffineExpr
   collectCoefficients(expr);
 
+  // Prev coeff
+  //llvm::dbgs() << "Coeff: ";
+  //for (auto coeff : coefficients) {
+  //  llvm::dbgs() << std::get<0>(coeff) << ", ";
+  //}
+  //llvm::dbgs() << "\n";
+
   // Step 2: Modify the coefficients based on the updated_position_index
   SmallVector<std::tuple<int64_t, unsigned>, 4> modifiedCoefficients;
-  int64_t targetCoefficient = std::get<0>(coefficients[updated_position_index]);
+  int64_t targetCoefficient = std::get<0>(coefficients[coefficients.size() -1 - updated_position_index]);
 
   for (int i = 0; i < static_cast<int>(coefficients.size()); ++i) {
     int64_t coeff = std::get<0>(coefficients[i]);
     unsigned position = std::get<1>(coefficients[i]);
-
     if (coeff > targetCoefficient)
       coeff = (coeff / upperBound) * paddedUpperBound;
     modifiedCoefficients.push_back(std::make_tuple(coeff, position));
   }
 
-  std::function<mlir::AffineExpr(mlir::AffineExpr, int &)> rebuildExprWithUpdatedCoefficients = 
-      [&](mlir::AffineExpr expr, int &coefficientIndex) -> mlir::AffineExpr {
+  // Updated coeff
+  //llvm::dbgs() << "Coeff: ";
+  //for (auto coeff : modifiedCoefficients) {
+  //  llvm::dbgs() << std::get<0>(coeff) << ", ";
+  //}
+  //llvm::dbgs() << "\n";
+
+  std::function<mlir::AffineExpr(mlir::AffineExpr)> rebuildExprWithUpdatedCoefficients =
+      [&](mlir::AffineExpr expr) -> mlir::AffineExpr {
     if (auto binExpr = llvm::dyn_cast<mlir::AffineBinaryOpExpr>(expr)) {
-      // Recursively rebuild the binary expressions with updated coefficients
-      mlir::AffineExpr lhs = rebuildExprWithUpdatedCoefficients(binExpr.getLHS(), coefficientIndex);
-      mlir::AffineExpr rhs = rebuildExprWithUpdatedCoefficients(binExpr.getRHS(), coefficientIndex);
-      
-      // Return either Add or Mul based on the original binary operation
-      auto result = binExpr.getKind() == mlir::AffineExprKind::Add ? lhs + rhs : lhs * rhs; 
-      //llvm::errs() << "result: " << result << "\n";
+      mlir::AffineExpr lhs = rebuildExprWithUpdatedCoefficients(binExpr.getLHS());
+      mlir::AffineExpr rhs = rebuildExprWithUpdatedCoefficients(binExpr.getRHS());
+
+      auto result = binExpr.getKind() == mlir::AffineExprKind::Add ? lhs + rhs : lhs * rhs;
       return result;
     } else if (auto dimExpr = llvm::dyn_cast<mlir::AffineDimExpr>(expr)) {
-      // For dimension expressions, multiply by the updated coefficient
-      int64_t new_coefficient = std::get<0>(modifiedCoefficients[coefficientIndex]);
-      unsigned position = std::get<1>(modifiedCoefficients[coefficientIndex]);
-  
-      // Make sure we update only the right dimension, skip if the dimension doesn't match
-      if (dimExpr.getPosition() == position) {
-        coefficientIndex++; // Increment the coefficient index
-        auto expr = mlir::getAffineDimExpr(position, context) * mlir::getAffineConstantExpr(new_coefficient, context);
-        return expr;
+      for (const auto& entry : modifiedCoefficients) {
+        int64_t coeff;
+        unsigned int position;
+        std::tie(coeff, position) = entry;
+        if (position == dimExpr.getPosition()) {
+          auto expr = mlir::getAffineDimExpr(position, context) * mlir::getAffineConstantExpr(coeff, context);
+          return expr;
+        }
       }
+      llvm::errs() << "Failed to update dimension \"" << dimExpr << "\"";
+      return dimExpr;
     } else if (auto constExpr = llvm::dyn_cast<mlir::AffineConstantExpr>(expr)) {
-      coefficientIndex++;
       return mlir::getAffineConstantExpr(1, context);
     }
     return expr; // Return the original expression if no specific case matches
   };
 
-  int coefficientIndex = 0;
-  return rebuildExprWithUpdatedCoefficients(expr, coefficientIndex);
+  return rebuildExprWithUpdatedCoefficients(expr);
 }
 
-int TestLoopPadding::findLoopVariableIndexInApplyOp(mlir::affine::AffineApplyOp applyOp, mlir::Value loopVar) {
-  auto operands = applyOp.getOperands();
+int TestLoopPadding::findLoopVariableIndex(mlir::Value loopVar) {
+  int index = 0;
 
-  for (unsigned i = 0; i < operands.size(); ++i) {
-    if (operands[i] == loopVar) {
-      return i;
+  for (const auto& entry : loopInfoMap) {
+    const auto& [upperBound, stepSize, storedValue] = entry;
+    if (storedValue == loopVar) {
+      return index;
     }
+    ++index;
   }
   return -1;
 }
 
-AffineMap TestLoopPadding::updateAffineMapWithNewExpr(AffineMap oldMap, 
-                                           mlir::AffineExpr updatedExpr, 
-                                           unsigned exprIndex, 
-                                           mlir::MLIRContext *context) {
+AffineMap TestLoopPadding::updateAffineMapWithNewExpr(AffineMap& oldMap,
+                                           mlir::AffineExpr updatedExpr,
+                                           unsigned exprIndex,
+                                           mlir::MLIRContext *context,
+                                           mlir::affine::AffineForOp& forOp) {
   llvm::SmallVector<mlir::AffineExpr, 4> exprs(oldMap.getResults().begin(), oldMap.getResults().end());
 
   if (exprIndex < exprs.size()) {
     exprs[exprIndex] = updatedExpr;
   } else {
-    llvm::errs() << "Failed to update AffineMap\n"; 
+    llvm::errs() << "Failed to update AffineMap\n";
     return oldMap;
   }
+  paddedMapInfo[oldMap.getAsOpaquePointer()].insert(forOp.getAsOpaquePointer());
   AffineMap newMap = AffineMap::get(oldMap.getNumDims(), oldMap.getNumSymbols(), exprs, context);
+  paddedMapInfo[newMap.getAsOpaquePointer()] = paddedMapInfo[oldMap.getAsOpaquePointer()];
+  auto it = paddedMapInfo.find(oldMap.getAsOpaquePointer());
+  if (it != paddedMapInfo.end()) {
+    paddedMapInfo.erase(it);  // Erase the map and its associated loops
+  } else {
+    llvm::errs() << "AffineMap not found in the map.\n";
+  }
   return newMap;
 }
 
