ac55376acbd27981e3e631bcc0706345eeb880b7
Yunseon Shin <ysshin@postech.ac.kr>
2025-02-14 15:10:08 +0000

    [PyTorchSim] tag supports kenrel stride

--- mlir/test/lib/Conversion/PyTorchSimToVCIX/TestPyTorchSimToVCIXConversion.cpp
+++ mlir/test/lib/Conversion/PyTorchSimToVCIX/TestPyTorchSimToVCIXConversion.cpp
@@ -362,30 +362,48 @@ struct MatmulOpLowering : public OpRewritePattern<linalg::MatmulOp> {
     }
     int ADimOffset = numAccumulationLoops;
     int BDimOffset = numAccumulationLoops;
-    if (innerLoops.size()==4) {
+    if (innerLoops.size()==4) { // innerloop : K_H, K_W, O_H, O_W
       /* FIXME. this is totally heuristic based lowering... */
-      ADimOffset = innerLoops.size() + numAccumulationLoops;
-      BDimOffset = ADimOffset-2;
-      int64_t oW, kW, iW;
-      oW = getLoopUpperBound(innerLoops.at(3));
-      kW = getLoopUpperBound(innerLoops.at(1));
-      iW = oW + kW - 1;
-      ATagExpr = ATagExpr + \
-        rewriter.getAffineDimExpr(ADimOffset-4)*((K/KStep)*(M/MStep)*iW) + \
-        rewriter.getAffineDimExpr(ADimOffset-3)*((K/KStep)*(M/MStep)) + \
-        rewriter.getAffineDimExpr(ADimOffset-2)*((K/KStep)*(M/MStep)*iW) + \
-        rewriter.getAffineDimExpr(ADimOffset-1)*((K/KStep)*(M/MStep));
-      BTagExpr = BTagExpr + \
-        rewriter.getAffineDimExpr(BDimOffset-2)*((N/NStep)*(K/KStep)*kW) + \
-        rewriter.getAffineDimExpr(BDimOffset-1)*((N/NStep)*(K/KStep));
-      /* Add Operands */
+      Value k_h = innerLoops.at(0).getInductionVar();
+      Value k_w = innerLoops.at(1).getInductionVar();
+      Value o_h = innerLoops.at(2).getInductionVar();
+      Value o_w = innerLoops.at(3).getInductionVar();
+      // find affine.apply using loop index (k_h, k_w, o_h, o_w)
+      int64_t offset_w = 1;
+      affineForOp = innerLoops.at(3);
+      affineForOp->walk([&](mlir::Operation *nestedOp) {
+        if (auto affineApplyOp = llvm::dyn_cast_or_null<affine::AffineApplyOp>(nestedOp)) {
+          if (affineApplyOp.getOperand(0) == o_w && affineApplyOp.getOperand(1) == k_w) {
+            AffineMap map = affineApplyOp.getAffineMap();
+            AffineExpr expr = map.getResult(0);
+            expr.walk([&](AffineExpr subExpr) {
+              if (auto constExpr = subExpr.dyn_cast<AffineConstantExpr>()) {
+                offset_w = constExpr.getValue();
+              }
+            });
+          }
+        }
+      });
       ATagOperands.push_back(innerLoops.at(0).getInductionVar());
       ATagOperands.push_back(innerLoops.at(1).getInductionVar());
       ATagOperands.push_back(innerLoops.at(2).getInductionVar());
       ATagOperands.push_back(innerLoops.at(3).getInductionVar());
-
       BTagOperands.push_back(innerLoops.at(0).getInductionVar());
       BTagOperands.push_back(innerLoops.at(1).getInductionVar());
+      ADimOffset = ATagOperands.size();
+      BDimOffset = BTagOperands.size();
+      int64_t oW, kW;
+      kW = getLoopUpperBound(innerLoops.at(1));
+      oW = getLoopUpperBound(innerLoops.at(3));
+      int64_t offset_h = 1 + (oW - 1) * offset_w + (kW - 1);
+      ATagExpr = ATagExpr + \
+        rewriter.getAffineDimExpr(ADimOffset-4)*((K/KStep)*(M/MStep)*offset_h) + \
+        rewriter.getAffineDimExpr(ADimOffset-3)*((K/KStep)*(M/MStep)) + \
+        rewriter.getAffineDimExpr(ADimOffset-2)*((K/KStep)*(M/MStep)*offset_h) + \
+        rewriter.getAffineDimExpr(ADimOffset-1)*((K/KStep)*(M/MStep)*offset_w);
+      BTagExpr = BTagExpr + \
+        rewriter.getAffineDimExpr(BDimOffset-2)*((N/NStep)*(K/KStep)*kW) + \
+        rewriter.getAffineDimExpr(BDimOffset-1)*((N/NStep)*(K/KStep));
     }
     ATagExpr = ATagExpr + rewriter.getAffineDimExpr(ADimOffset)*(M/MStep) + \
       rewriter.getAffineDimExpr(ADimOffset+1).floorDiv((MStep+SYSTOLIC_SIZE-1)/SYSTOLIC_SIZE);
